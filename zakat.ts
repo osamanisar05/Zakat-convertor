ab37b6de858830fbef01aa0cb3579d3b84e3aec9
osamanisar05 <164087967+osamanisar05@users.noreply.github.com>
2024-04-20 12:12:03 +0500

    convert your gold value (tola) in zakat amount

---
+++ zakat.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let zakat: number = 2.5 ;
+
+let goldValueInTola = 234800;
+
+let goldWeightInTolaReq = 7.5;
+
+let condition = true;
+
+while (condition){
+    const answer = await inquirer.prompt(
+        [
+            {
+                name: "goldWeight",
+                message: "Enter your Gold Weight in Tola",
+                type: "number"
+            }
+        ]
+    );
+    
+    let goldWeight = answer.goldWeight;
+    
+    if (goldWeight >= goldWeightInTolaReq){
+        let calculationZakat = (goldWeight * goldValueInTola) * (zakat/100);
+        console.log(`The calculated value of gold weighing ${goldWeight} tola is ${calculationZakat}`);
+    }
+    else {
+        console.log("Zakat not applicable");
+    }
+    condition = false;
+}
